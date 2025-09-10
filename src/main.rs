3d5bded62a85b3e17d804004b71d0e499626b397
JosephR2004 <joseph_rojas82231@elpoli.edu.co>
2025-09-10 13:44:07 -0500

    servidor montado

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+
+
+// Manejador de ruta
+async fn hello()-> impl Responder{
+    "hello, world!"
+}
+// Macro directiva que modifica una funcion
+
+#[actix_web::main]
+async fn main()-> std::io::Result<()>{
+    // Inicia el servidor web
+    HttpServer::new(||{
+        App::new()
+        // Ruta y su correspondiente manejador
+        .route("/", web::get().to(hello))
+    })    
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
